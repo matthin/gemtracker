ad4d2d9a14d5861bc6331b9412c07ebee7ed00b4
Justin Harrison <justin@matthin.com>
2015-03-08 16:52:15 -0400

    Add GemInfo wrapper

--- Source/Client.cc
+++ Source/Client.cc
@@ -8,6 +8,42 @@ Client::Client() {
     
 }
 
+GemInfo
+Client::getGemInfo(const std::string& gemName) {
+    sf::Http http(baseURL);
+    const sf::Http::Request request(
+        "/api/v1/gems/" + gemName + ".json"
+    );
+    const auto response = http.sendRequest(request);
+    const auto root = initRoot(response);
+
+    std::unordered_map<std::string,
+                       std::vector<GemInfo::Dependency>> dependencies;
+    dependencies["development"] = parseDependencies(
+        root["dependencies"]["development"]
+    );
+    dependencies["runtime"] = parseDependencies(
+        root["dependencies"]["runtime"]
+    );
+
+    return GemInfo(
+        root["name"].asString(),
+        root["downloads"].asInt(),
+        root["version"].asString(),
+        root["versionDownloads"].asInt(),
+        root["authors"].asString(),
+        root["info"].asString(),
+        root["projectURI"].asString(),
+        root["gemURI"].asString(),
+        root["homepageURI"].asString(),
+        root["wikiURI"].asString(),
+        root["documentationURI"].asString(),
+        root["sourceCodeURI"].asString(),
+        root["bugtrackerURI"].asString(),
+        dependencies
+    );
+}
+
 Downloads
 Client::getDownloads(const char* gemName, const char* gemVersion) {
     return getDownloads(std::string(gemName), std::string(gemVersion));
@@ -26,6 +62,18 @@ Client::getDownloads(const std::string& gemName,
         root["version_downloads"].asInt(),
         root["total_downloads"].asInt()
     );
+}
+
+std::vector<GemInfo::Dependency>
+Client::parseDependencies(const Json::Value& dependencies) {
+    std::vector<GemInfo::Dependency> parsedDependencies;
+    for (const auto dependency : dependencies) {
+        parsedDependencies.push_back(
+            GemInfo::Dependency(dependency["name"].asString(),
+                                dependency["requirements"].asString())
+        );
+    }
+    return parsedDependencies;
 }
 
 } // namespace GemTracker
--- Source/Client.h
+++ Source/Client.h
@@ -2,9 +2,52 @@
 
 #include <string>
 #include <SFML/Network.hpp>
+#include <unordered_map>
+#include <vector>
 #include "json/json.h"
 
 namespace GemTracker {
+
+struct GemInfo{
+    struct Dependency {
+        Dependency(const std::string name, const std::string requirements)
+                : name(name), requirements(requirements) {}
+        const std::string name;
+        const std::string requirements;
+    };
+
+    GemInfo(const std::string name, const int downloads,
+            const std::string version, const int versionDownloads,
+            const std::string authors, const std::string info,
+            const std::string projectURI, const std::string gemURI,
+            const std::string homepageURI, const std::string wikiURI,
+            const std::string documentationURI,
+            const std::string sourceCodeURI,
+            const std::string bugTrackerURI,
+            const std::unordered_map<std::string,
+                                     std::vector<Dependency>> dependencies)
+            : name(name), downloads(downloads), version(version),
+            versionDownloads(versionDownloads), authors(authors), info(info),
+            projectURI(projectURI), gemURI(gemURI), homepageURI(homepageURI),
+            wikiURI(wikiURI), documentationURI(documentationURI),
+            sourceCodeURI(sourceCodeURI), bugTrackerURI(bugTrackerURI),
+            dependencies(dependencies) {}
+    const std::string name;
+    const int downloads;
+    const std::string version;
+    const int versionDownloads;
+    const std::string authors;
+    const std::string info;
+    const std::string projectURI;
+    const std::string gemURI;
+    const std::string homepageURI;
+    const std::string wikiURI;
+    const std::string documentationURI;
+    const std::string sourceCodeURI;
+    const std::string bugTrackerURI;
+    const std::unordered_map<std::string,
+                             std::vector<Dependency>> dependencies;
+};
 
 struct Downloads {
     Downloads(const int version, const int total)
@@ -16,6 +59,8 @@ struct Downloads {
 class Client {
 public:
     Client();
+
+    GemInfo getGemInfo(const std::string& gemName);
 
     Downloads getDownloads(
         const char* gemName, const char* gemVersion
@@ -33,6 +78,9 @@ private:
     }
 
     static constexpr char baseURL[] = "http://rubygems.org/";
+
+    std::vector<GemInfo::Dependency>
+    parseDependencies(const Json::Value& dependencies);
 };
 
 } // namespace GemTracker
