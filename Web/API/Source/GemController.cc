028d7d0578a2ede700164f9f9b9b765b02b0a4f1
Justin Harrison <justin@matthin.com>
2015-03-13 16:39:39 -0400

    Return list of all gems

--- Web/API/Source/GemController.cc
+++ Web/API/Source/GemController.cc
@@ -1,5 +1,9 @@
 #include "GemController.h"
 
+#include <SQLiteCpp/Statement.h>
+#include <SQLiteCpp/Database.h>
+#include <json/json.h>
+
 namespace GemTracker {
 
 void GemController::index(const Http::Request& request,
@@ -7,7 +11,15 @@ void GemController::index(const Http::Request& request,
     response->headers["Content-Type"] = "application/json";
     response->code = 200;
     response->codeMessage = "OK";
-    response->setMessage("Wut up bruv?");
+
+    SQLite::Database db("GemTracker.sqlite3", SQLITE_OPEN_READWRITE);
+    SQLite::Statement query(db, "SELECT id FROM gem");
+
+    Json::Value root;
+    while (query.executeStep()) {
+        root.append(Json::Value(query.getColumn(0).getInt()));
+    }
+    response->setMessage(root.toStyledString());
 }
 
 } // namespace GemTracker
