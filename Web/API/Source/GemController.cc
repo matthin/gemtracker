27ecfa60189e3e1e811646c259e1e23b84dbee5e
Justin Harrison <justin@matthin.com>
2015-03-26 07:15:49 -0400

    gem#index shows name

--- Web/API/Source/GemController.cc
+++ Web/API/Source/GemController.cc
@@ -14,11 +14,14 @@ void GemController::index(const Http::Request& request,
     response->codeMessage = "OK";
 
     SQLite::Database db("GemTracker.sqlite3", SQLITE_OPEN_READWRITE);
-    SQLite::Statement query(db, "SELECT id FROM gem");
+    SQLite::Statement query(db, "SELECT id, name FROM gem");
 
     Json::Value root;
     while (query.executeStep()) {
-        root.append(Json::Value(query.getColumn(0).getInt()));
+        Json::Value entry;
+        entry["id"] = query.getColumn(0).getInt();
+        entry["name"] = query.getColumn(1).getText();
+        root.append(entry);
     }
     response->setMessage(root.toStyledString());
 }
